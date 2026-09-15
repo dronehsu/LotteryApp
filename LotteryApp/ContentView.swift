75d6417fada7be98d721e93172e709d645bad9c6
JUNGPING <taiwandronehsu@gmail.com>
2026-09-15 16:01:33 +0800

    Initial Commit

---
+++ LotteryApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LotteryApp
+//
+//  Created by 徐榮彬 on 2026/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LotteryApp/LotteryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotteryAppApp.swift
+//  LotteryApp
+//
+//  Created by 徐榮彬 on 2026/9/15.
+//
+
+import SwiftUI
+
+@main
+struct LotteryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
